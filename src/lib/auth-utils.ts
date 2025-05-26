a500e03a740aacf82e73eea4e6e45f114940af9b
henryympark <henry.ympark@gmail.com>
2025-05-26 09:12:58 +0900

    Delete lib/auth-utils.ts - fully migrated to domains/auth

--- src/lib/auth-utils.ts
+++
@@ -1,7 +0,0 @@
-// This file has been moved to src/domains/auth/services/authUtils.ts
-// Please use: import { getUserRole, ... } from "@/domains/auth";
-
-export {};
-
-// Re-export for backward compatibility (temporary)
-export * from "@/domains/auth";
